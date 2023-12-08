8c6e9b2fc4adc5c594f3b2d1853533527d9d29d3
kume1a <kumela011@gmail.com>
2023-12-08 19:48:59 +0400

    use shared prefs for storing auth keys

--- lib/app/di/modules/api_client/api_client_store_module.dart
+++ lib/app/di/modules/api_client/api_client_store_module.dart
@@ -1,11 +1,11 @@
 import 'package:findx_dart_client/app_client.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:injectable/injectable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 @module
 abstract class ApiClientStoreModule {
   @lazySingleton
-  AuthTokenStore authTokenStore(FlutterSecureStorage flutterSecureStorage) {
-    return SecureStoreageTokenStoreImpl(flutterSecureStorage);
+  AuthTokenStore authTokenStore(SharedPreferences sharedPreferences) {
+    return SharedPrefsTokenStoreImpl(sharedPreferences);
   }
 }
